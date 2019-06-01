71931e96aa1957ed751cb220243031c73f4536ab
João Regis <joao.reegiis@gmail.com>
2019-06-01 16:21:21 -0300

    dashboard and search

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CurriculosService } from './../../services/curriculos.service';
+import { Component, OnInit } from '@angular/core';
+import { Curriculo } from 'src/app/models/curriculo';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  users: Observable<Curriculo[]>;
+
+  constructor(
+    private curriculosService: CurriculosService
+  ) { }
+
+  onSearch(value) {
+
+    console.log(value);
+
+    if (value === '') {
+
+      this.users = this.curriculosService.getCurriculos();
+
+    } else {
+
+      this.users = this.curriculosService.filtrarCurriculos(value);
+
+    }
+
+  }
+
+  ngOnInit() {
+
+    this.users = this.curriculosService.getCurriculos();
+  }
 
 }
--- src/app/services/curriculos.service.ts
+++ src/app/services/curriculos.service.ts
@@ -2,7 +2,7 @@ import { Curriculo } from './../models/curriculo';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestoreCollection, AngularFirestore, DocumentReference } from '@angular/fire/firestore';
-import { map, take } from 'rxjs/operators';
+import { map, take, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +26,30 @@ export class CurriculosService {
           });
         })
       );
+
+  }
+
+  filtrarCurriculos(search: string): Observable<Curriculo[]> {
+
+    return this.curriculosCollection.snapshotChanges().pipe(
+      map(items => items.filter(item => {
+
+        return (
+          item.payload.doc.data().cargo.toLowerCase().includes(search.toLowerCase())
+          || item.payload.doc.data().nome.toLowerCase().includes(search.toLowerCase())
+          || item.payload.doc.data().sobrenome.toLowerCase().includes(search.toLowerCase())
+        );
+
+      })),
+      filter(items => items && items.length > 0),
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        });
+      })
+    );
 
   }
 
